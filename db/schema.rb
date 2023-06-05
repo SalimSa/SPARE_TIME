e58cf373b1df42cffa02954875700a4eb13ae475
jonah michael <ymichael81@hotmail.com>
2023-06-05 18:10:44 +0100

    component model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_170350) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_05_170855) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_170350) do
     t.datetime "updated_at", null: false
     t.index ["theme_id"], name: "index_collaborations_on_theme_id"
     t.index ["user_id"], name: "index_collaborations_on_user_id"
+  end
+
+  create_table "components", force: :cascade do |t|
+    t.text "content"
+    t.string "type"
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_components_on_item_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -66,6 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_170350) do
 
   add_foreign_key "collaborations", "themes"
   add_foreign_key "collaborations", "users"
+  add_foreign_key "components", "items"
   add_foreign_key "items", "topics"
   add_foreign_key "themes", "users"
   add_foreign_key "topics", "themes"
