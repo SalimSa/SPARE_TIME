5ebfc168e777ea2ad520ec2d3f5aeb1aa4b1bf7d
jonah michael <ymichael81@hotmail.com>
2023-06-05 18:00:10 +0100

    collaboration and models created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_164118) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_05_165909) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "collaborations", force: :cascade do |t|
+    t.integer "role"
+    t.bigint "user_id", null: false
+    t.bigint "theme_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["theme_id"], name: "index_collaborations_on_theme_id"
+    t.index ["user_id"], name: "index_collaborations_on_user_id"
+  end
 
   create_table "themes", force: :cascade do |t|
     t.string "title"
@@ -21,6 +31,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_164118) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_themes_on_user_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "theme_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["theme_id"], name: "index_topics_on_theme_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,5 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_164118) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "collaborations", "themes"
+  add_foreign_key "collaborations", "users"
   add_foreign_key "themes", "users"
+  add_foreign_key "topics", "themes"
 end
