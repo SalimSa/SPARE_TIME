3593720b9f40130167e0cb8a71458390dc202b65
SalimSa <salim.saadi85@gmail.com>
2023-06-07 15:43:50 +0100

    adding more seeds for the themes, topics and items

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,18 +22,23 @@ user3 = User.create(email: "salim@gmail.com", password: "123456")
 puts 'Creating themes...'
 travel = Theme.new(title: "Travel", description: "Best places to travel to !", user_id: 1)
 travel.save!
+
 fooding = Theme.new(title: "Fooding", description: "Best places whereeat !", user_id: 2)
 fooding.save!
+
 adventure = Theme.new(title: "Adventure Escapes", description: "Experience thrilling adventures in breathtaking locations.", user_id: 2)
 adventure.save!
+
 artistic = Theme.new(title: "Artistic Exploration", description: "Immerse yourself in the vibrant world of art and creativity.", user_id: 2)
 artistic.save!
+
 wellness = Theme.new(title: "Wellness Retreats", description: "Rejuvenate your mind, body, and soul with soothing wellness retreats.", user_id: 2)
 wellness.save!
+
 historical = Theme.new(title: "Historical Wonders", description: "Discover the fascinating history and architectural wonders of different eras.", user_id: 2)
 historical.save!
 
-puts 'Creating topics...'
+puts 'Creating topics for travel...'
 cuba = Topic.new(title: "Cuba", description: "Cuba’s capital is almost 500 years old and a riot of color.", theme: travel)
 cuba.save!
 bali = Topic.new(title: "Bali", description: "Indonesian most  beaches, volcanoes, Komodo dragons and jungles sheltering elephants and tigers", theme: travel)
@@ -42,6 +47,8 @@ maldives = Topic.new(title: "The Maldives", description: "This tropical nation i
 maldives.save!
 tokyo = Topic.new(title: "Tokyo", description: "Visiting Tokyo is like visiting the future—flashing neon lights, incredible technology—yet there’s still a rich sense of culture and history.", theme: travel)
 tokyo.save!
+
+puts 'Creating topics for fooding...'
 ramen = Topic.new(title: "Ramen", description: "If not tried, you must have heard of Ramen at least once in your life.", theme: fooding)
 ramen.save!
 pho = Topic.new(title: " Pho", description: "A simple yet an incredible dish, Pho (pronounced as 'fuh') is a Vietnamese dish made of rice noodles and meat", theme: fooding)
@@ -51,30 +58,38 @@ paella.save!
 apfelstrudel = Topic.new(title: "Apfelstrudel", description: "More commonly known as Apple Strudel, Apfelstrudel is a popular pastry in Austria and other parts of Europe.", theme: fooding)
 apfelstrudel.save!
 
+puts 'Creating topics for historical...'
 ancientrome = Topic.new(title: "Ancient Rome", description: "Explore the fascinating history and ruins of Ancient Rome.", theme: historical)
 ancientrome.save!
 machupicchu = Topic.new(title: "Machu Picchu", description: "Discover the ancient Inca city nestled in the Andes Mountains.", theme: historical)
 machupicchu.save!
+greet = Topic.new(title: "Great Wall of China", description: "Discover the majestic Great Wall of China and its historical significance.", theme: historical)
+greet.save!
+collosseum = Topic.new(title: "The Colosseum", description: "Explore the iconic Roman amphitheater and delve into its ancient history.", theme: historical)
+collosseum.save!
+
+puts 'Creating topics for wellness...'
 yoga = Topic.new(title: "Yoga Retreat", description: "Immerse yourself in a peaceful yoga retreat surrounded by nature.", theme: wellness)
 yoga.save!
 meditation = Topic.new(title: "Meditation Retreat", description: "Find inner calm and serenity in a tranquil meditation retreat.", theme: wellness)
 meditation.save!
+zen = Topic.new(title: "Zen Retreat in Kyoto", description: "Immerse yourself in a peaceful Zen retreat in the serene gardens of Kyoto.", theme: wellness)
+zen.save!
+ayurveda = Topic.new(title: "Ayurvedic Retreat in Kerala", description: "Experience traditional Ayurvedic healing in a rejuvenating retreat in Kerala.", theme: wellness)
+ayurveda.save!
 
-# Create topics for Artistic Exploration
+puts 'Create topics for Artistic Exploration'
 parisiens = Topic.new(title: "Parisian Art Scene", description: "Experience the vibrant art scene and museums of Paris.", theme: artistic)
 parisiens.save!
+louvre = Topic.new(title: "The Louvre Museum", description: "Indulge in artistic exploration at the world-renowned Louvre Museum in Paris.", theme: artistic)
+louvre.save!
+streetArt = Topic.new(title: "Street Art in Berlin", description: "Discover the vibrant street art scene in Berlin and its unique expressions.", theme: artistic)
+streetArt.save!
+streetArtTour = Topic.new(title: "Street Art Tour", description: "Discover hidden street art gems in various cities around the world.", theme: artistic)
+streetArtTour.save!
 
-street = Topic.new(title: "Street Art Tour", description: "Discover hidden street art gems in various cities around the world.", theme: artistic)
-street.save!
 
-# Create topics for Adventure Escapes
-hiking = Topic.new(title: "Hiking in Patagonia", description: "Embark on an exhilarating hiking adventure in the stunning Patagonian landscapes.", theme: travel)
-hiking.save!
-
-safari = Topic.new(title: "Safari in Serengeti", description: "Witness incredible wildlife and go on a thrilling safari in the Serengeti.", theme: travel)
-safari.save!
-
-puts 'Creating items...'
+puts 'Creating items for Cuba'
 havana = Item.new(title: "Havana Oulala", description: "is a super cozy place at the bottom of Calle Muralla, just a stone through from the Plaza Vieja.", topic: cuba)
 havana.save!
 eldandy = Item.new(title: "El Dandy", description: "favorite places has opened its doors to seated guests again!", topic: cuba)
