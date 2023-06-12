c74064301672a8495217e2d6fe536308a63575f0
SalimSa <salim.saadi85@gmail.com>
2023-06-12 19:49:34 +0100

    fixing validation on item components

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,8 @@ class Item < ApplicationRecord
   # has_many :components, dependent: :destroy
   has_one_attached :photo
   has_many :locations, dependent: :destroy
+  has_many :task_components, dependent: :destroy
+  has_many :text_components, dependent: :destroy
   def start_time
     self.calendar_component.start
   end
--- db/migrate/20230612144851_drop_calendars.rb
+++
@@ -1,5 +0,0 @@
-class DropCalendars < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :calendars
-  end
-end
