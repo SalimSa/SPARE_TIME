c7a759fc73026c39571dc09012b9f3f19d878e0d
pierre robertson <pierrerobertson06@gmail.com>
2023-06-30 18:05:20 +0200

    i added crud to the cards

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,7 @@
 class TopicsController < ApplicationController
   before_action :set_theme, only: [:new, :create]
+  before_action :set_topic, only: [:edit, :update, :destroy, :show]
+
 
   def index
     @topics = Topic.all
@@ -20,31 +22,33 @@ class TopicsController < ApplicationController
   end
 
   def show
-    @topic = Topic.find(params[:id])
     @items = @topic.items
     @item = Item.new
   end
 
-  # def edit
-  # end
+  def edit
+  end
 
-  # def update
-  #   if @topic.update(topic_params)
-  #     redirect_to topic_path(@topic)
-  #   else
-  #     render :new, status: :unprocessable_entity
-  #   end
-  # end
+  def update
+    if @topic.update(topic_params)
+      redirect_to topic_path(@topic)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-  # def destroy
-  #   @topic.destroy
-  #   redirect_to topics_path, status: :see_other
-  # end
-
+  def destroy
+    @topic.destroy
+    redirect_to theme_path(@topic.theme), status: :see_other
+  end
 
   private
   def set_theme
     @theme = Theme.find(params[:theme_id])
+  end
+
+  def set_topic
+    @topic = Topic.find(params[:id])
   end
 
   def topic_params
